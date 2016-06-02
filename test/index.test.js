9d4b6fce2750a88bbf5ae4bdbbacb9a126ee9ed5
Nathan Houle <nathan@nathanhoule.com>
2016-06-02 12:29:19 -0700

    Modernize test harness; remove Duo support, add Browserify support

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,9 @@
+'use strict';
 
-var Analytics = require('analytics.js-core').constructor;
-var integration = require('analytics.js-integration');
-var sandbox = require('clear-env');
-var tester = require('analytics.js-integration-tester');
+var Analytics = require('@segment/analytics.js-core').constructor;
+var integration = require('@segment/analytics.js-integration');
+var sandbox = require('@segment/clear-env');
+var tester = require('@segment/analytics.js-integration-tester');
 var GTM = require('../lib/');
 
 describe('Google Tag Manager', function() {
